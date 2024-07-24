c6f78fbb4e41d83ca10df9cf880ec984c3806339
Kevin Carrogan <kevin@carrogan.com>
2024-07-24 15:40:16 +0100

    Allow recusion of folders

--- folders.py
+++ folders.py
@@ -4,7 +4,19 @@ from files import blame_file
 def blame_folder(repo, tree):
     total = 0
     user_totals = {}
+    sub_trees = []
     blobs = []
+
+    for sub_tree in tree.trees:
+        total_from_tree, aggregated_users_totals, sub_items = blame_folder(repo, sub_tree)
+        total += total_from_tree
+        for user, user_total in aggregated_users_totals:
+            user_totals.setdefault(user, 0)
+            user_totals[user] += user_total
+        sub_trees.append(
+            (sub_tree.name, (total_from_tree, aggregated_users_totals), sub_items),
+        )
+
     for blob in tree.blobs:
         total_from_blob, aggregated_users_totals = blame_file(repo, blob)
         total += total_from_blob
@@ -15,4 +27,4 @@ def blame_folder(repo, tree):
             (blob.name, (total_from_blob, aggregated_users_totals)),
         )
 
-    return (total, list(user_totals.items()), blobs)
+    return (total, list(user_totals.items()), sub_trees + blobs)
--- tests/test_blame_folder.py
+++ tests/test_blame_folder.py
@@ -34,3 +34,41 @@ def test_blame_folder(git_repo):
             ("test.txt", (3, [("Tony Stark <tony@avengers.com>", 2), ("Steve Rogers <captain@avengers.com>", 1)])),
         ],
     )
+
+
+def test_blame_folder_with_sub_folders(git_repo):
+    path = git_repo.workspace
+    folder = path / "folder"
+    folder.mkdir()
+    top_file = folder / "top.txt"
+    top_file.write_text("test\n")
+    git_repo.api.config_writer().set_value("user", "name", "Tony Stark").release()
+    git_repo.api.config_writer().set_value("user", "email", "tony@avengers.com").release()
+    git_repo.api.index.add(top_file)
+    git_repo.api.index.commit("Add file")
+
+    sub_folder = folder / "sub_folder"
+    sub_folder.mkdir()
+    bottom_file = sub_folder / "bottom.txt"
+    bottom_file.write_text("test\n")
+    git_repo.api.config_writer().set_value("user", "name", "Steve Rogers").release()
+    git_repo.api.config_writer().set_value("user", "email", "captain@avengers.com").release()
+    git_repo.api.index.add(bottom_file)
+    git_repo.api.index.commit("Add another file")
+
+    tree = git_repo.api.tree()["folder"]
+
+    assert blame_folder(git_repo.api, tree) == (
+        2,
+        [
+            ("Steve Rogers <captain@avengers.com>", 1),
+            ("Tony Stark <tony@avengers.com>", 1)],
+        [
+            (
+                "sub_folder",
+                (1, [("Steve Rogers <captain@avengers.com>", 1)]),
+                [("bottom.txt", (1, [("Steve Rogers <captain@avengers.com>", 1)]))],
+            ),
+            ("top.txt", (1, [("Tony Stark <tony@avengers.com>", 1)])),
+        ],
+    )
