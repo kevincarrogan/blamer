50872247320ae6aecc31c7a5f1c555a59b44150d
Kevin Carrogan <kevin@carrogan.com>
2024-08-01 20:04:48 +0100

    Add file tree

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,100 @@
+import PropTypes from "prop-types";
+
 import "./App.css";
+import { DiCss3, DiJavascript, DiNpm } from "react-icons/di";
+import { FaList, FaRegFolder, FaRegFolderOpen } from "react-icons/fa";
+import TreeView, { flattenTree } from "react-accessible-treeview";
+
+const folder = {
+  name: "",
+  children: [
+    {
+      name: "src",
+      children: [{ name: "index.js" }, { name: "styles.css" }],
+    },
+    {
+      name: "node_modules",
+      children: [
+        {
+          name: "react-accessible-treeview",
+          children: [{ name: "index.js" }],
+        },
+        { name: "react", children: [{ name: "index.js" }] },
+      ],
+    },
+    {
+      name: ".npmignore",
+    },
+    {
+      name: "package.json",
+    },
+    {
+      name: "webpack.config.js",
+    },
+  ],
+};
+
+const data = flattenTree(folder);
+
+const FolderIcon = ({ isOpen }) =>
+  isOpen ? (
+    <FaRegFolderOpen color="e8a87c" className="icon" />
+  ) : (
+    <FaRegFolder color="e8a87c" className="icon" />
+  );
+
+FolderIcon.propTypes = {
+  isOpen: PropTypes.boolean.isRequired,
+};
+
+const FileIcon = ({ filename }) => {
+  const extension = filename.slice(filename.lastIndexOf(".") + 1);
+  switch (extension) {
+    case "js":
+      return <DiJavascript color="yellow" className="icon" />;
+    case "css":
+      return <DiCss3 color="turquoise" className="icon" />;
+    case "json":
+      return <FaList color="yellow" className="icon" />;
+    case "npmignore":
+      return <DiNpm color="red" className="icon" />;
+    default:
+      return null;
+  }
+};
+
+FileIcon.propTypes = {
+  filename: PropTypes.string.isRequired,
+};
 
 function App() {
-  return <>TREE</>;
+  return (
+    <div>
+      <div className="directory">
+        <TreeView
+          data={data}
+          aria-label="directory tree"
+          nodeRenderer={({
+            element,
+            isBranch,
+            isExpanded,
+            getNodeProps,
+            level,
+          }) => (
+            <div {...getNodeProps()} style={{ paddingLeft: 20 * (level - 1) }}>
+              {isBranch ? (
+                <FolderIcon isOpen={isExpanded} />
+              ) : (
+                <FileIcon filename={element.name} />
+              )}
+
+              {element.name}
+            </div>
+          )}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
